abfa3f0f27c1adda21964a0fe964acaf2a552851
William Troup <williamtroup@gmail.com>
2024-03-04 10:54:40 +0000

    Updated to v0.5.0.

--- dist/jhson.js
+++ dist/jhson.js
@@ -1,4 +1,4 @@
-/*! JHson.js v0.4.0 | (c) Bunoon 2024 | MIT License */
+/*! JHson.js v0.5.0 | (c) Bunoon 2024 | MIT License */
 (function() {
   function getJSON(element, properties) {
     var result = _string.empty;
@@ -415,7 +415,7 @@
     return this;
   };
   this.getVersion = function() {
-    return "0.4.0";
+    return "0.5.0";
   };
   (function(documentObject, windowObject, jsonObject, mathObject) {
     _parameter_Document = documentObject;

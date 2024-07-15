a9f98c6a4cc1e513e0d64cdcdf9a39e025ffe495
William Troup <williamtroup@gmail.com>
2024-07-15 11:06:16 +0100

    Added start of TS files.

---
+++ src/jhson.ts
@@ -0,0 +1,29 @@
+/**
+ * JHson.js
+ * 
+ * A JavaScript library for converting between HTML and JSON, with binding, templating, attributes, and CSS support.
+ * 
+ * @file        jhson.ts
+ * @version     v2.0.0
+ * @author      Bunoon
+ * @license     MIT License
+ * @copyright   Bunoon 2024
+ */
+
+
+import { Constants } from "./ts/constant"
+
+
+type StringToJson = {
+    parsed: boolean;
+    object: any;
+};
+
+
+( () => {
+
+
+    ( () => {
+    
+    } )();
+} )();
---
+++ src/ts/constant.ts
@@ -0,0 +1,16 @@
+/**
+ * JHson.js
+ * 
+ * A JavaScript library for converting between HTML and JSON, with binding, templating, attributes, and CSS support.
+ * 
+ * @file        constant.js
+ * @version     v2.0.0
+ * @author      Bunoon
+ * @license     MIT License
+ * @copyright   Bunoon 2024
+ */
+
+
+export namespace Constants {
+    export const JHSON_JS_ATTRIBUTE_NAME = "data-jhson-js";
+}
